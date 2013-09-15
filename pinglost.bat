bd44cbbe259e1e9706ec81fb31031060c4758086
ebergeron <ber.emile@gmail.com>
2013-09-15 04:13:36 -0400

    + option parsing + help menu + default output + basic doc

--- pinglost.bat
+++ pinglost.bat
@@ -1,46 +1,151 @@
 @echo off
-GOTO pingloststart
+GOTO documentationend
 /////////////////////////
 DOCUMENTATION
 
+Author: E. Bergeron
+Website: www.prismalstudio.com
+Email: contact@prismalstudio.com
 
+Options parsing provided by dbenham on stackoverflow.com
+http://stackoverflow.com/a/8162578/1218980
 
+Useful when you want to know when a packet was lost. Use it to ping 
+multiple hostname and see if both were losing packets at the same time.
+
+For help:
+pinglost /?
 
 /////////////////////////
+:documentationend
+	setlocal enabledelayedexpansion
+	
+	:: Can't run without param
+	IF "%~1"=="" (
+		echo Use the "/?" flag for help
+		exit /b
+	)
+	:: Default help call
+	IF "%~1"=="/?" (
+		call :help
+		exit /b
+	)
+	
+	:: start the program
+	goto :pingloststart
+		
+:help
+	:: Called when the flag /? is the first param
+	echo.
+	echo The pinglost command sends a ping request to "hostname_or_IP" then logs the
+	echo lost packets into a file called "Ping_hostname_AAAA-MM-DD_HHhMM.txt" with
+	echo a timestamp.
+	echo.
+	echo call: pinglost hostname_or_IP [-n count] [-l size] [-w timeout]
+	echo.
+	echo -n count         Specifies the number of Echo Request messages sent.
+	echo                  The default is 4.
+	echo -l size          Specifies the length, in bytes, of the Data field in 
+	echo                  the Echo Request messages sent. The default is 32. 
+	echo                  The maximum size is 65,527.
+	echo -w timeout       Specifies the amount of time, in milliseconds, to wait 
+	echo                  for the Echo Reply message that corresponds to a given 
+	echo                  Echo Request message to be received. If the Echo Reply 
+	echo                  message is not received within the time-out, the "Request 
+	echo                  timed out" error message is displayed. The default 
+	echo                  time-out is 4000 (4 seconds).
+	echo.
+	echo The parameters following the hostname can be in any order.
+	exit /b
+	:: Define the option names along with default values, using a <space>
+	:: delimiter between options.
+	::
+	:: Each option has the format -name:[default]
+	::
+	:: The option names are NOT case sensitive.
+	::
+	:: Options that have a default value expect the subsequent command line
+	:: argument to contain the value. If the option is not provided then the
+	:: option is set to the default. If the default contains spaces, contains
+	:: special characters, or starts with a colon, then it should be enclosed
+	:: within double quotes. The default can be undefined by specifying the
+	:: default as empty quotes "".
+	:: NOTE - defaults cannot contain * or ? with this solution.
+	::
+	:: Options that are specified without any default value are simply flags
+	:: that are either defined or undefined. All flags start out undefined by
+	:: default and become defined if the option is supplied.
+	::
+	:: The order of the definitions is not important.
+	:: example:
+	:: set "options=-n:4 -option2:"" -option3:"three word default" -help: -flag2:"
+	::
 :pingloststart
-setlocal enabledelayedexpansion
-set hostIP=%1
+	set "options=-n:4 -l:32 -w:4000"
 
-REM Set Filename with Date and Time
-For /f "tokens=1-4 delims=- " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
-For /f "tokens=1-2 delims=:" %%a in ('time /t') do (set mytime=%%ah%%b)
+	:: Set the default option values
+	for %%O in (%options%) do for /f "tokens=1,* delims=:" %%A in ("%%O") do set "%%A=%%~B"
 
-set startTimeStamp=%mydate%_%mytime%
-set filename=Ping_%hostIP%_%startTimeStamp%
+:loopoptions
+	:: Validate and store the options, one at a time, using a loop.
+	:: Options start at arg 3 in this example. Each SHIFT is done starting at
+	:: the first option so required args are preserved.
+	::
+	if not "%~2"=="" (
+		set "test=!options:*%~2:=! "
+		if "!test!"=="!options! " (
+			rem No substitution was made so this is an invalid option.
+			rem Error handling goes here.
+			rem I will simply echo an error message.
+			echo Error: Invalid option %~2
+		) else if "!test:~0,2!"==" " (
+			rem Set the flag option using the option name.
+			rem The value doesn't matter, it just needs to be defined.
+			set "%~2=1"
+		) else (
+			rem Set the option value using the option as the name.
+			rem and the next arg as the value
+			set "%~2=%~3"
+			shift /2
+		)
+		shift /2
+		goto :loopoptions
+	)
+	
+:pinglostcommand
+	set hostIP=%1
 
-REM file creation and setting the header
-echo USAGE: pinglog [host or IP] [number of ping] > %filename%.txt
-echo Only lost packets are logged. >> %filename%.txt
-echo Ping started at %startTimeStamp% >> %filename%.txt
-echo -------------------------------------- >> %filename%.txt
+	:: Set Filename with Date and Time
+	For /f "tokens=1-4 delims=- " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
+	For /f "tokens=1-2 delims=:" %%a in ('time /t') do (set mytime=%%ah%%b)
 
-REM iterate x times, where x is %2
-for /l %%x in (2, 1, %2) do (
-	call :pingcommand
-	REM hack to wait 1 second approx., works on every PC
-	PING -n 2 127.0.0.1>nul 
-)
+	set startTimeStamp=%mydate%_%mytime%
+	set filename=Ping_%hostIP%_%startTimeStamp%
+
+	:: file creation and setting the header
+	echo Use "/?" for help. > %filename%.txt
+	echo Only lost packets are logged. >> %filename%.txt
+	echo Ping to %hostIP% started at %startTimeStamp% with options: >> %filename%.txt
+	set - >> %filename%.txt
+	echo -------------------------------------- >> %filename%.txt
+
+	:: iterate x times, where x is %2
+	for /l %%x in (2, 1, %-n%) do (
+		call :pingfunction
+		REM hack to wait 1 second approx., works on every PC
+		PING -n 2 127.0.0.1>nul 
+	)
 :exit /b
 
 REM FUNCTION the ping command and echoing only lost packets
-:pingcommand
-set pingline=1
-for /f "delims=" %%A in ('ping -n 1 -w 500 -l 255 %hostIP%') do (
-	if !pingline! equ 2 (
-		set logline=!date! !time! "%%A"
-		echo !logline! | find "TTL=">nul || echo !logline! >> %filename%.txt
+:pingfunction
+	set pingline=1
+	for /f "delims=" %%A in ('ping -n 1 -w %-w% -l %-l% %hostIP%') do (
+		if !pingline! equ 2 (
+			set logline=!date! !time! "%%A"
+			echo !logline! | find "TTL=">nul || echo !logline! >> %filename%.txt
+			)
+		set /a pingline+=1
 		)
-	set /a pingline+=1
-	)
-echo %logline%
-exit /b
+	echo %logline%
+	exit /b
